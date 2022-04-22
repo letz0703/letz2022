c99da027f790045678f775c40d8d0d5a7848a38e
letz <letz0703@gmail.com>
2022-04-22 22:25:23 +0900

    logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navigation from './components/navigation/navigation'
+import Logo from './components/logo/Logo'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Logo />
     </div >
   );
 }
 
 export default App;
+
