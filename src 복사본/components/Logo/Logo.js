d1fe7d2e8a8a613e3dc3583ce7ded1e6e92b24bd
letz0703 <letz0703@gmail.com>
2022-04-25 11:49:39 +0900

    0425월

---
+++ src 복사본/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+// import React from "react";
+// import wizard from "wizard.png";
+
+// const Logo = () => {
+//   return (
+//     <div>
+//       <img src="{wizard}" alt="wizard" />
+//     </div>
+//   )
+// };
+
+// export default Logo;
