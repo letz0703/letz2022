6cd6c6662ece0d10062fee39cae41544ad48e569
letz <letz0703@gmail.com>
2022-04-26 20:45:53 +0900

    Rank
    Rank.js & Font-family

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navigation from './components/navigation/navigation'
 import Logo from './components/logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
     </div >
   );
