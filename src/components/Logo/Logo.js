44b929aa34f9c6492931977db6617d6f7d7c8cbc
letz <letz0703@gmail.com>
2022-04-29 00:12:55 +0900

    fix Tilt Error w/ DOM

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,21 +1,22 @@
 import React from "react";
-import Tilt from "react-tilt"
+// import Tilt from "react-tilt"
 import "./Logo.css"
 import wizard from "./wizard.png"
 
 const Logo = () => {
   return (
-    <div className="m4 mb4">
-      <Tilt className="Tilt ml6 br2 shadow-2 center"
+    <div className="m4 mb4 ">
+      {/* <Tilt className="Tilt ml6 br2 shadow-2 center" */}
+      <div className="ml6 br2 shadow-2 center"
         options={{ max: 10 }}
-        style={{ height: 100, width: 100 }}
+        style={{ height: 175, width: 175 }}
       >
-        <div className="Tilt-inner pa3">
+        <div className="Tilt-inner pa3 bg-orange">
           <img src={wizard} alt="logo"
-            style={{ paddingTop: '5px', width: '100px' }}
+            style={{ paddingTop: '5px', width: 175 }}
           />
         </div>
-      </Tilt>
+      </div>
     </div>
   )
 };
