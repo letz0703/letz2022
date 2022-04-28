feded82b7d58a38f6ad89287863f228cb5804427
letz <letz0703@gmail.com>
2022-04-28 23:29:24 +0900

    set state & respond to typing

--- src/App.js
+++ src/App.js
@@ -13,6 +13,16 @@ const particlesInit = async (main) => {
 };
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
   render() {
     return (
       <div className="App">
@@ -20,7 +30,7 @@ class App extends React.Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} />
       </div >
     );
   }
